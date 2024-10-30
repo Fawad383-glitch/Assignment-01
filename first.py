78796bfe77336e953763f7289ff2d53946043aae
Fawad383-glitch <fawad.jan2007@gmail.com>
2024-10-30 18:49:05 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,39 @@
+num1=20
+num2=40
+num3=60
+print(num1+num2+num3) # for addition
+num1=20
+num2=40
+num3=60
+print(num1-num2-num3) # for subtraction
+num1=20
+num2=40
+num3=60
+print(num1*num2*num3) # for multiply
+num1=120
+num2=-80
+num3=48000
+print(num1+num2+num3) # total sum
+num=48040
+print(num/3) # average
+
+# finding area of the following shapes
+
+"rectangle"
+length=50
+width=34
+area=print(length*width)
+
+
+"triangle"
+base=60
+height=30
+area=print(0.5*base*height)
+
+"circle" 
+radius=4
+area=print(3.14159*(radius**2))
+
+"square"  
+side=40
+area=print(side**2)
